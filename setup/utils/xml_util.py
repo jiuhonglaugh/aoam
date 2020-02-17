527327a1534edc42d880b9d2a72c41b5a0c1dcc8
久鸿 <34256397+jiuhonglaugh@users.noreply.github.com>
2020-02-17 11:50:55 +0800

    1

--- setup/utils/xml_util.py
+++ setup/utils/xml_util.py
@@ -3,8 +3,6 @@
 import xml.etree.ElementTree as ET
 from setup.utils import file_util
 
-configPath = file_util.repairPath('../config/')
-
 
 def getXml(filePath, lable):
     tree = ET.parse(filePath)  # open
@@ -15,7 +13,7 @@ def getXml(filePath, lable):
     return list
 
 
-def getHadoopXml():
+def getHadoopXml(configPath):
     hadoopXml = {}
     tmp = getXml(configPath + 'hdfs-site.xml', 'property')
     hadoopXml['dfs.http.address'] = tmp.get('dfs.http.address')
@@ -28,7 +26,7 @@ def getHadoopXml():
     hadoopXml['yarn.resourcemanager.webapp.address'] = tmp.get('yarn.resourcemanager.webapp.address')
     hadoopXml['yarn.resourcemanager.hostname'] = tmp.get('yarn.resourcemanager.hostname')
     hadoopXml['yarn.log.server.web-service.url'] = tmp.get('yarn.log.server.web-service.url')
-    hadoopXml['http://test:19888/jobhistory/logs'] = tmp.get('http://test:19888/jobhistory/logs')
+    hadoopXml['yarn.log.server.url'] = tmp.get('yarn.log.server.url')
 
     tmp = getXml(configPath + 'mapred-site.xml', 'property')
     hadoopXml['mapreduce.jobhistory.address'] = tmp.get('mapreduce.jobhistory.address')
@@ -37,6 +35,64 @@ def getHadoopXml():
     return hadoopXml
 
 
+def getDataNodeAndNodeManager(path):
+    dict = {}
+    with open(file_util.repairPath(path) + 'slaves') as r:
+        for line in r.readlines():
+            dict[line.strip()] = 'DataNode,NodeManager'
+    return dict
+
+
+def getNameNode(keys, dict):
+    key = keys.get('dfs.http.address').split(':')[0]
+    if key in dict:
+        dict[key] = dict[key] + ',NameNode'
+    else:
+        dict[key] = 'NameNode'
+    return dict
+
+
+def getSecondNameNode(keys, dict):
+    key = keys.get('dfs.namenode.secondary.http-address').split(':')[0]
+    if key in dict:
+        dict[key] = dict[key] + ',SecondaryNameNode'
+    else:
+        dict[key] = 'SecondaryNameNode'
+    return dict
+
+
+def getResourceManager(keys, dict):
+    key = keys.get('yarn.resourcemanager.hostname')
+    if key in dict:
+        dict[key] = dict[key] + ',ResourceManager'
+    else:
+        dict[key] = 'ResourceManager'
+    return dict
+
+
+def getHistoryServer(keys, dict):
+    key = keys.get('yarn.log.server.url').split(":")[1].replace('//', '')
+    if key in dict:
+        dict[key] = dict[key] + ',JobHistoryServer'
+    else:
+        dict[key] = 'JobHistoryServer'
+    return dict
+
+
+def checkServerProcess():
+    list = getHadoopXml(file_util.repairPath('../config/'))
+    serverProcess = getDataNodeAndNodeManager('../config/')
+    serverProcess = getNameNode(list, serverProcess)
+    serverProcess = getSecondNameNode(list, serverProcess)
+    serverProcess = getResourceManager(list, serverProcess)
+    serverProcess = getHistoryServer(list, serverProcess)
+    return serverProcess
+
+
+def updateDict(olds, news):
+    for old in news:
+        print(old)
+
+
 if __name__ == '__main__':
-    list = getHadoopXml()
-    print(list)
+    print(checkServerProcess())
