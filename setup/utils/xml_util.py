09173e045459d9c77b0f37a30f907f93f2bd69ad
久鸿 <34256397+jiuhonglaugh@users.noreply.github.com>
2020-02-17 10:32:45 +0800

    1

--- setup/utils/xml_util.py
+++ setup/utils/xml_util.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 # -*- encoding:utf-8 -*-
 import xml.etree.ElementTree as ET
+from setup.utils import file_util
+
+configPath = file_util.repairPath('../config/')
 
 
 def getXml(filePath, lable):
@@ -14,12 +17,23 @@ def getXml(filePath, lable):
 
 def getHadoopXml():
     hadoopXml = {}
-    tmp = getXml('../config/hdfs-site.xml', 'property')
+    tmp = getXml(configPath + 'hdfs-site.xml', 'property')
     hadoopXml['dfs.http.address'] = tmp.get('dfs.http.address')
     hadoopXml['dfs.namenode.secondary.http-address'] = tmp.get('dfs.namenode.secondary.http-address')
 
-    tmp = getXml('../config/core-site.xml', 'property')
+    tmp = getXml(configPath + 'core-site.xml', 'property')
     hadoopXml['fs.defaultFS'] = tmp.get('fs.defaultFS')
+
+    tmp = getXml(configPath + 'yarn-site.xml', 'property')
+    hadoopXml['yarn.resourcemanager.webapp.address'] = tmp.get('yarn.resourcemanager.webapp.address')
+    hadoopXml['yarn.resourcemanager.hostname'] = tmp.get('yarn.resourcemanager.hostname')
+    hadoopXml['yarn.log.server.web-service.url'] = tmp.get('yarn.log.server.web-service.url')
+    hadoopXml['http://test:19888/jobhistory/logs'] = tmp.get('http://test:19888/jobhistory/logs')
+
+    tmp = getXml(configPath + 'mapred-site.xml', 'property')
+    hadoopXml['mapreduce.jobhistory.address'] = tmp.get('mapreduce.jobhistory.address')
+    hadoopXml['mapreduce.jobhistory.webapp.address'] = tmp.get('mapreduce.jobhistory.webapp.address')
+
     return hadoopXml
 
 
