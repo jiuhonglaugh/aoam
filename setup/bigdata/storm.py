86e1e2e6bc4296799de7460f9e07efce5b81fd6e
zhangsongqing <zhangsongqing1319@gmail.com>
2020-02-18 17:29:20 +0800

    storm

--- setup/bigdata/storm.py
+++ setup/bigdata/storm.py
@@ -8,20 +8,19 @@ import re
 import os
 from elasticsearch import Elasticsearch
 
-conf = config_util.getDict('elasticsearch')
-log = logger(loggername='elasticsearch')
+conf = config_util.getDict('storm')
+log = logger()
 
 '''
 检测索引是否存在，如果集群挂了，是无法返回索引的存在与否
-'''
 
 
 def checkServer():
     hostAndPorts = conf.get('hosts')
     es = Elasticsearch(hostAndPorts)
-    # print(es.indices.exists(index="test"))  #填写对应索引
+    #print(es.indices.exists(index="test"))  #填写对应索引
     lists = es.cluster.health()
-    logger.info("elasticsearch集群状态：")
+    logger.info("elasticsearch集群状态：",end="")
     ES_cluster_status = lists["status"]
     if ES_cluster_status == "green":
         logger.info("####集群处于健康状态####")
@@ -29,11 +28,11 @@ def checkServer():
         logger.info("集群处于亚健康状态")
     elif ES_cluster_status == "red":
         logger.warn("集群挂了")
-    logger.info("elasticsearch集群节点数：" + lists['number_of_nodes'])
-    logger.info("elasticsearch集群节点数：" + lists['number_of_data_nodes'])
+    logger.info("elasticsearch集群节点数："+lists['number_of_nodes'])
+    logger.info("elasticsearch集群节点数："+lists['number_of_data_nodes'])
+'''
 
-
-def getES(hostAndPorts):
+def getStorm(hostAndPorts):
     dict = {}
     for hostAndPort in hostAndPorts:
         host = hostAndPort.split(':')[0]
@@ -41,25 +40,25 @@ def getES(hostAndPorts):
     return dict
 
 
-def startES(host, server):
-    ES_HOME = os.getenv('ELASTICSEARCH_HOME')
+def startStorm(host, server):
+    STORM_HOME = os.getenv('STORM_HOME')
     log.warn('开始启动 ' + host + ' 节点的 ' + server + ' 服务\n')
-    _shell = 'ansible client -l ' + host + ' -a "' + ES_HOME + '/start.sh"'
+    _shell = 'ansible client -l ' + host + ' -a "' + STORM_HOME + '/start.sh"'
     exeCmd.run(_shell)
 
 
 def checkServerProcess():
     hostAndPorts = conf.get('hosts')
-    serverlist = getES(hostAndPorts.split(','))
+    serverlist = getStorm(hostAndPorts.split(','))
     for host in serverlist:
         content = exeCmd.Popen('ansible client -l ' + host + ' -a "jps"')
         if (len(re.findall(serverlist.get(host), content)) < 1):
-            log.warn(host + ' 节点的 ' + 'Elasticsearch' + ' 服务未运行')
-            startES(host, 'Elasticsearch')
+            log.warn(host + ' 节点的 ' + 'Storm' + ' 服务未运行')
+            startStorm(host, 'Storm')
         else:
-            log.info(host + ' 节点的 ' + ' Elasticsearch ' + '服务正在运行')
+            log.info(host + ' 节点的 ' + ' Storm ' + '服务正在运行')
 
 
 if __name__ == '__main__':
     checkServerProcess()
-    checkServer()
+    #checkServer()
